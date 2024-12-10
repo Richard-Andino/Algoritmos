ce5cc7a7a5cb3a9acd9cf3936014a6fb6aacdc75
Richard-Andino <99428432+Richard-Andino@users.noreply.github.com>
2024-12-10 00:08:44 -0600

    Add files via upload

---
+++ Guía de ejercicios/Ejercicio4.py
@@ -0,0 +1,10 @@
+#Ejercicio 4 buscar un elemento en un arreglo
+
+import Funciones
+maxSize = 10          
+arr = Funciones.Funciones(maxSize) 
+
+array=[1,2,3,4,5,6,7,8,9,10]
+item=12
+
+arr.buscar(array, item)
