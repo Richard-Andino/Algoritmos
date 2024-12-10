59e1e2b03ea65a7c9e51f36bcae9f34792e4bffb
Richard-Andino <99428432+Richard-Andino@users.noreply.github.com>
2024-12-10 00:11:27 -0600

    Update Funciones.py

--- Guía de ejercicios/Funciones.py
+++ Guía de ejercicios/Funciones.py
@@ -1,14 +1,57 @@
 
 class Funciones(object):
-    def __init__(self, tamaño):
-        self.Array=[0] * tamaño
     
+    def __init__(self, initialSize):
+        self.__a = [None] * initialSize
+        self.__nItems = 0
+  
     def len(self):
-        return self.tamaño
+        return self.__nItems
+  
+    def get(self, n):
+        if 0 <= n and n < self.__nItems: 
+         return self.__a[n]
+  
+    def set(self, n, value):
+        if 0 <= n and n < self.__nItems:
+            self.__a[n] = value
+
+    def insertar(self, item):
+        self.__a[self.__nItems] = item
+        self.__nItems += 1
     
+    def eliminar(self, item):
+        for j in range(self.__nItems):
+            if self.__a[j] == item:
+                self.__nItems -= 1
+                for k in range(j, self.__nItems): 
+                    self.__a[k] = self.__a[k+1]
+                return True
+        return False   
     
-    def invertir_Array():
-        new_Array=[]
-        for i in range(len(Array),0,-1):
-            new_Array.append(i)
-        print("El arreglo invertido es: ", new_Array)
+    def traverse(self, function=print):
+        for j in range(self.__nItems):
+            function(self.__a[j])
+            
+    def invertir_Array(self, array):
+        new_Array = Funciones(len(array))
+        for a in range(len(array) - 1, -1, -1):
+            new_Array.insertar(array[a])
+        return new_Array
+    
+    def buscar(self, array, item):
+        for a in array:
+            if item==a:
+                print("El numero está presente en el arreglo")
+                return
+        print("El numero no está presente en el arreglo")
+        
+    def eliminar_duplicado(self, array):
+        new_Array = Funciones(len(array))
+        for item in array:
+            if item not in new_Array.__a:
+                new_Array.insertar(item)
+        return new_Array
+                
+
+        
