ea618a96b895c6ebc61357d4449c4dddeb063a90
Richard-Andino <99428432+Richard-Andino@users.noreply.github.com>
2024-12-10 21:40:05 -0600

    Add files via upload

---
+++ Guía de ejercicios/Ejercicio6.py
@@ -0,0 +1,22 @@
+import FuncionesPila
+
+pila=FuncionesPila.Funciones(maxSize=10)
+
+pila.push(5)
+pila.push(8)
+pila.push(9)
+pila.push(1)
+print("Pila")
+pila.traverse()
+
+print("Aplicando pop")
+pila.pop()
+pila.traverse()
+
+print("Aplicando peek")
+pila.peek
+
+pila.traverse()
+
+
+
